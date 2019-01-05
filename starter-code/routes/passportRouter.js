287e6d0bfccd4d2d60a2c971315ed136d30a1c0b
Camille Chapleau <camille.chapleau@hotmail.fr>
2019-01-05 16:48:14 +0100

    Camille Chapleau Berlin

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,89 @@
 const express        = require("express");
 const passportRouter = express.Router();
+const ensureLogin = require("connect-ensure-login");
+
 // Require user model
-
 // Add bcrypt to encrypt passwords
-
 // Add passport 
 
+// User model
+const User = require("../models/user");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  })
+  .catch(error => {
+    next(error)
+  })
+});
+
+authRoutes.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+authRoutes.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+authRoutes.get("/private", ensureLogin.ensureLoggedIn('/auth/login'), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+authRoutes.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+module.exports = authRoutes;
 
 const ensureLogin = require("connect-ensure-login");
 
+router.get('/signup', (req, res, next) => {
+  res.render('/passport/signup');
+});
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+authRoutes.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("private", { user: req.user });
 });
 
 module.exports = passportRouter;
